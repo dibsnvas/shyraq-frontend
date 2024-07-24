04453278578c53506e3b58fa5d28d9d008b10ace
dibsnvas <d_beisenbayeva@kbtu.kz>
2024-07-24 20:37:01 +0500

    fixed homepage

--- src/Page/HomePage.tsx
+++ src/Page/HomePage.tsx
@@ -15,7 +15,7 @@ const HomePage: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-customBlue text-white font-custom p-4">
       <h1 className="text-5xl md:text-9xl font-bold text-customYellow">SHYRAQ</h1>
-      <p className="text-xl md:text-2xl mt-4 font-serif text-customYellow">Учите и переводите с языка жестов в реальном времени здесь</p>
+      <p className="text-xl md:text-2xl mt-4 font-serif text-customYellow mb-6">Учите и переводите с языка жестов в реальном времени здесь</p>
       <div className="grid-container">
         <div className="grid">
           <Draggable bounds="parent">
