b2810a35af997da9495956ac5e6349105db7e606
dibsnvas <d_beisenbayeva@kbtu.kz>
2024-07-30 11:44:38 +0500

    Animation

--- src/Page/HomePage.tsx
+++ src/Page/HomePage.tsx
@@ -13,59 +13,61 @@ import Dot from '../homepage/dot';
 
 const HomePage: React.FC = () => {
   return (
-<div className="flex flex-col items-center justify-center min-h-screen bg-customBlue text-white font-need p-4">
-<h1 className="text-8xl md:text-[13rem] font-need text-customYellow">SHYRAQ</h1>
-<p className="text-xl md:text-2xl mt-4 font-serif text-customYellow mb-6 text-center md:text-left">Учите и переводите с языка жестов в реальном времени здесь</p>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-customBlue text-white font-need p-4">
+      <h1 className="text-6xl md:text-[9rem] font-custom text-customYellow">SHYRAQ</h1>
+      <p className="text-xl md:text-2xl mt-4 font-serif text-customYellow mb-6 text-center md:text-left">
+        Учите и переводите с языка жестов в реальном времени здесь
+      </p>
       <div className="grid-container">
         <div className="grid">
           <Draggable bounds="parent">
-            <div className="semi_circle_square_cont">
+            <div className="semi_circle_square_cont draggable">
               <SemiCircleSquare />
             </div>
           </Draggable>
           <Draggable bounds="parent">
-            <div className="four_circles_cont">
+            <div className="four_circles_cont draggable">
               <FourCircles />
             </div>
           </Draggable>
           <Draggable bounds="parent">
-            <div className="circle_cont">
+            <div className="circle_cont draggable">
               <Circle />
             </div>
           </Draggable>
           <Draggable bounds="parent">
-            <div className="half_circle_cont">
+            <div className="half_circle_cont draggable">
               <HalfCircle />
             </div>
           </Draggable>
           <Draggable bounds="parent">
-            <div className="quarter_circle_cont">
+            <div className="quarter_circle_cont draggable">
               <QuarterCircle />
             </div>
           </Draggable>
           <Draggable bounds="parent">
-            <div className="semi_circle_bottom_cont">
+            <div className="semi_circle_bottom_cont draggable">
               <SemiCircleBottom />
             </div>
           </Draggable>
           <Draggable bounds="parent">
-            <div className="circle_with_dot_cont">
+            <div className="circle_with_dot_cont draggable">
               <CircleWithDot />
             </div>
           </Draggable>
           <Draggable bounds="parent">
-            <div className="half_square_cont">
+            <div className="half_square_cont draggable">
               <HalfSquare />
             </div>
           </Draggable>
           <Draggable bounds="parent">
-            <div className="dot_cont">
-              <Dot />
+            <div className="dot_cont draggable">
+            <QuarterCircle />
             </div>
           </Draggable>
           <Draggable bounds="parent">
-            <div className="half_square_cont">
-              <HalfSquare />
+            <div className="four_circles_cont draggable">
+              <FourCircles />
             </div>
           </Draggable>
         </div>
